c890820b092f0902676d58e5ebd465c2e61db530
Iresh Dissanayaka <socialexpz1@gmail.com>
2020-02-20 15:47:35 +0530

    initial implementation

--- index.js
+++ index.js
@@ -1,19 +1,59 @@
-/**
- * This is the main entrypoint to your Probot app
- * @param {import('probot').Application} app
- */
+const dotenv = require("dotenv");
+dotenv.config();
+const simpleGit = require("simple-git")(process.env.WORKSPACE);
+
 module.exports = app => {
-  // Your code here
-  app.log('Yay, the app was loaded!')
+  app.on("pull_request.opened", async context => {
+    console.log(context.payload);
+    cloneCommit(context);
+    startSonarQubeScan(context);
+    updatePendingStatus(context);
+  });
 
-  app.on('issues.opened', async context => {
-    const issueComment = context.issue({ body: 'Thanks for opening this issue!' })
-    return context.github.issues.createComment(issueComment)
-  })
+  const router = app.route("/my-app");
 
-  // For more information on building apps:
-  // https://probot.github.io/docs/
+  router.get("/hello-world", (req, res) => {
+    res.send("Hello Wordl");
+  });
+};
 
-  // To get your app running against GitHub, see:
-  // https://probot.github.io/docs/development/
+function cloneCommit(context) {
+  const cloneUrl = context.payload.pull_request.base.repo.clone_url;
+  const ref = context.payload.pull_request.head.ref;
+  simpleGit
+    .clone(cloneUrl, ["--single-branch", `--branch=${ref}`, "--depth=1"])
+    .then(() => console.log("done cloning"));
+}
+
+function startSonarQubeScan(context) {
+  const repoName = context.payload.pull_request.base.repo.name;
+  const mvn = require("maven").create({
+    cwd: `${process.env.WORKSPACE}/${repoName}`
+  });
+
+  mvn
+    .execute(["clean", "install", "sonar:sonar"], { skipTests: true })
+    .then(() => {
+      console.log("done");
+    });
+}
+
+function updatePendingStatus(payloadContext) {
+  const owner = payloadContext.payload.pull_request.head.repo.owner.login;
+  const repo = payloadContext.payload.pull_request.head.repo.name;
+  const sha = payloadContext.payload.pull_request.head.sha;
+  const state = "pending";
+  const description = "CI Test - Check quality of code";
+  const context = "SonarQube Quality Gate";
+
+  const payload = {
+    owner,
+    repo,
+    sha,
+    state,
+    description,
+    context
+  };
+
+  payloadContext.github.repos.createStatus(payload);
 }

5ce64b3a94fceecd284c0b32bf95a66e267a1a26
iresh <socialexpz1@gmail.com>
2020-02-21 11:36:36 +0000

    fix - event listener accumilation

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 const EventEmitter = require('events');
 
-class QualityGate extends EventEmitter { }
-
-const qualityGate = new QualityGate();
+class QualityGate extends EventEmitter {}
 
 const bodyParser = require('body-parser');
 const rimraf = require("rimraf");
@@ -11,88 +9,97 @@ dotenv.config();
 const simpleGit = require("simple-git")(process.env.WORKSPACE);
 
 module.exports = app => {
-    app.on("pull_request.opened", async context => {
-        console.log(context.payload);
-        await cleanWorkspace();
-        await cloneCommit(context);
-        startSonarQubeScan(context);
-        setCommitStatus(context, 'pending');
+	const qualityGate = new QualityGate();
 
-        qualityGate.on('success', () => {
-            setCommitStatus(context, 'success');
-        });
+	app.on("pull_request.opened", async context => {
+		await cleanWorkspace();
+		await cloneCommit(context);
+		startSonarQubeScan(context);
+		setCommitStatus(context, 'pending');
 
-        qualityGate.on('failure', () => {
-            setCommitStatus(context, 'failure');
-        });
-    });
+		qualityGate.once(`${getCommitSha(context)}_success`, () => {
+			setCommitStatus(context, 'success');
+		});
 
-    const router = app.route("/");
-    router.use(bodyParser.json());
+		qualityGate.once(`${getCommitSha(context)}_failure`, () => {
+			setCommitStatus(context, 'failure');
+		});
+	});
 
-    router.get("/s", (req, res) => {
-        res.send("Hello Wordl");
-    });
+	const router = app.route("/");
+	router.use(bodyParser.json());
 
-    router.post("/s", (req, res) => {
-        updateQualityGateStatus(req.body);
-        res.send("Success");
-    });
+	router.get("/s", (req, res) => {
+		res.send("Hello Wordl");
+	});
+
+	router.post("/s", (req, res) => {
+		updateQualityGateStatus(req.body, qualityGate);
+		res.send("Success");
+	});
 };
 
 function cleanWorkspace() {
-    rimraf.sync(`${process.env.WORKSPACE}/*`);
+	rimraf.sync(`${process.env.WORKSPACE}/*`);
+}
+
+function getCommitSha(context) {
+	return context.payload.pull_request.head.sha;
 }
 
 async function cloneCommit(context) {
-    const cloneUrl = context.payload.pull_request.base.repo.clone_url;
-    const ref = context.payload.pull_request.head.ref;
-    await simpleGit.clone(cloneUrl, [
-        "--single-branch",
-        `--branch=${ref}`,
-        "--depth=1"
-    ]);
+	const cloneUrl = context.payload.pull_request.base.repo.clone_url;
+	const ref = context.payload.pull_request.head.ref;
+	await simpleGit.clone(cloneUrl, [
+		"--single-branch",
+		`--branch=${ref}`,
+		"--depth=1"
+	]);
 }
 
 function startSonarQubeScan(context) {
-    const repoName = context.payload.pull_request.base.repo.name;
-    const mvn = require("maven").create({
-        cwd: `${process.env.WORKSPACE}/${repoName}`
-    });
+	const repoName = context.payload.pull_request.base.repo.name;
+	const mvn = require("maven").create({
+		cwd: `${process.env.WORKSPACE}/${repoName}`
+	});
 
-    mvn
-        .execute(["clean", "install", "sonar:sonar"], { skipTests: true })
-        .then(() => {
-            console.log("done");
-        });
+	mvn
+		.execute(["clean", "install", "sonar:sonar"], {skipTests: true})
+		.then(() => {
+			console.log("done");
+		});
 }
 
-function updateQualityGateStatus(payload) {
-    const qualityGateStatus = payload.qualityGate.status;
-    console.log(qualityGate);
-    if (qualityGateStatus == 'OK') {
-        qualityGate.emit('success');
-    } else {
-        qualityGate.emit('failure');
-    }
+function updateQualityGateStatus(payload, qualityGate) {
+	console.log(payload);
+	const sha = payload.revision;
+	const qualityGateStatus = payload.qualityGate.status;
+	if (qualityGateStatus == 'OK') {
+		console.log("inside OK");
+		qualityGate.emit(`${sha}_success`);
+	} else {
+		console.log("inside FAILURE");
+		qualityGate.emit(`${sha}_failure`);
+	}
 }
 
 function setCommitStatus(payloadContext, commitStatus) {
-    const owner = payloadContext.payload.pull_request.head.repo.owner.login;
-    const repo = payloadContext.payload.pull_request.head.repo.name;
-    const sha = payloadContext.payload.pull_request.head.sha;
-    const state = commitStatus;
-    const description = "CI Test - Check quality of code";
-    const context = "SonarQube Quality Gate";
+	const owner = payloadContext.payload.pull_request.head.repo.owner.login;
+	const repo = payloadContext.payload.pull_request.head.repo.name;
+	const sha = payloadContext.payload.pull_request.head.sha;
+	const state = commitStatus;
+	const description = "CI Test - Check quality of code";
+	const context = "SonarQube Quality Gate";
 
-    const payload = {
-        owner,
-        repo,
-        sha,
-        state,
-        description,
-        context
-    };
+	const payload = {
+		owner,
+		repo,
+		sha,
+		state,
+		description,
+		context
+	};
 
-    payloadContext.github.repos.createStatus(payload);
+	console.log(payload);
+	payloadContext.github.repos.createStatus(payload);
 }
